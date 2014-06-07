b4e14755ee3fce13682ddc305ebc15145ea16560
Mary Lou <wilczogon@gmail.com>
2014-06-07 13:59:57 +0200

    hpp file of Processor_8086 upgraded

--- headers/Processor_8086.hpp
+++ headers/Processor_8086.hpp
@@ -9,84 +9,123 @@ public:
 	void AAM();
 	void AAS();
 	
-	void ADC();
-	void ADD();
-	void AND();
-	void CALL();
+	void ADC(char* arg1, char* arg2);
+	void ADD(char* arg1, char* arg2);
+	void AND(char* arg1, char* arg2);
+	void CALL(char* arg);
 	void CBW();
 	void CLC();
 	void CLD();
 	void CLI();
 	void CMC();
-	void CMP();
+	void CMP(char* arg1, char* arg2);
 	void CMPSB();
 	void CMPSW();
 	void CWD();
 	void DAA();
 	void DAS();
-	void DEC();
-	void DIV();
+	void DEC(char* arg);
+	void DIV(char* arg);
 	void ESC();
 	void HLT();
-	void IDIV();
-	void IMUL();
-	void IN();
-	void INC();
-	void INT();
+	void IDIV(char* arg);
+	void IMUL(char* arg);
+	void IN(char* arg1, char* arg2);
+	void INC(char* arg);
+	void INT(char* arg);
 	void INTO();
 	void IRET();
-	void Jcc();
-	void JCXZ();
-	void JMP();
+	
+	void JA(char* arg);
+	void JAE(char* arg);
+	void JB(char* arg);
+	void JBE(char* arg);
+	void JC(char* arg);
+	void JA(char* arg);
+	void JE(char* arg);
+	void JG(char* arg);
+	void JGE(char* arg);
+	void JL(char* arg);
+	void JLE(char* arg);
+	
+	void JO(char* arg);
+	void JP(char* arg);
+	void JPE(char* arg);
+	void JPO(char* arg);
+	void JS(char* arg);
+	void JZ(char* arg);
+	
+	void JCXZ(char* arg);
+	void JMP(char* arg);
 	void LAHF();
-	void LDS();
-	void LEA();
-	void LES();
-	void LOCK();
+	void LDS(char* arg1, char* arg2);
+	void LEA(char* arg1, char* arg2);
+	void LES(char* arg1, char* arg2);
+	void LOCK();	//TODO
 	void LODSB();
 	void LODSW();
-	void LOOP();
-	void LOOPx();
-	void MOV(char*, char*);
+	void LOOP(char* arg);
+	void LOOPE(char* arg);
+	void LOOPNE(char* arg);
+	void LOOPNZ(char* arg);
+	void LOOPNE(char* arg);
+	void LOOPZ(char* arg);
+	void MOV(char* arg1, char* arg2);
 	void MOVSB();
 	void MOVSW();
-	void MUL();
-	void NEG();
+	void MUL(char* arg);
+	void NEG(char* arg);
 	void NOP();
-	void NOT();
-	void OR();
-	void OUT();
-	void POP();
+	void NOT(char* arg);
+	void OR(char* arg1, char* arg2);
+	void OUT(char* arg1, char* arg2);
+	
+	void POP(char* arg);
 	void POPF();
-	void PUSH();
+	void PUSH(char* arg);
 	void PUSHF();
-	void RCL();
-	void RCR();
-	void REPxx();
+	
+	void RCL(char* arg1, char* arg2);
+	void RCR(char* arg1, char* arg2);
+	
+	void REP(char* arg);
+	void REPE(char* arg);
+	void REPNE(char* arg);
+	void REPNZ(char* arg);
+	void REPZ(char* arg);
+	
+	
 	void RET();
 	void RETN();
 	void RETF();
-	void ROL();
-	void ROR();
+	
+	void ROL(char* arg1, char* arg2);
+	void ROR(char* arg1, char* arg2);
+	
 	void SAHF();
-	void SAL();
-	void SAR();
-	void SBB();
+	void SAL(char* arg1, char* arg2);
+	void SAR(char* arg1, char* arg2);
+	
+	void SBB(char* arg1, char* arg2);
 	void SCASB();
 	void SCASW();
-	void SHL();
-	void SHR();
+	
+	void SHL(char* arg1, char* arg2);
+	void SHR(char* arg1, char* arg2);
+	
 	void STC();
 	void STD();
 	void STI();
+	
 	void STOSB();
 	void STOSW();
-	void SUB();
-	void TEST();
-	void WAIT();
-	void XCHG();
-	void XLAT();
-	void XOR();
+	
+	void SUB(char* arg1, char* arg2);
+	void TEST(char* arg1, char* arg2);
+	void WAIT();	// TODO
+	void XCHG(char* arg1, char* arg2);
+	void XLAT();	// TODO
+	void XOR(char* arg1, char* arg2);
 	
 	
 };
