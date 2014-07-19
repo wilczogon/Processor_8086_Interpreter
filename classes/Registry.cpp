057aa3bd66d1fbdffc38b51327c1e15d7b55ba3d
Mary Lou <wilczogon@gmail.com>
2014-07-19 14:25:29 +0200

    Registry files added.

---
+++ classes/Registry.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../headers/Registry.hpp"
+#include "../headers/Operand.hpp"
+
+
+Registry::Registry(char* expression): Operand(expression){
+}
